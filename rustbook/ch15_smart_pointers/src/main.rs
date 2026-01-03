5cdce2c55662fbdbdd5396d3745d2e5c20873673
David Hinrichs <dvd.hnrchs@gmail.com>
2026-01-03 23:34:47 +0100

    re-read on smart pointers

--- rustbook/ch15_smart_pointers/src/main.rs
+++ rustbook/ch15_smart_pointers/src/main.rs
@@ -19,14 +19,34 @@ fn hello(name: &str) {
     println!("Hello, {name}!");
 }
 
+// fn main() {
+//     let x = 5;
+//     let y = MyBox::new(x);
+
+//     assert_eq!(5, x);
+//     assert_eq!(5, *y);
+
+//     // Deref stuff
+//     let m = MyBox::new(String::from("Rust"));
+//     hello(&m);
+// }
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
 fn main() {
-    let x = 5;
-    let y = MyBox::new(x);
-
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
-
-    // Deref stuff
-    let m = MyBox::new(String::from("Rust"));
-    hello(&m);
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created");
 }
