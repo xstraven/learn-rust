eaab782367a62479e5e207e3dbff5bb205fb3d26
David Hinrichs <dvd.hnrchs@gmail.com>
2026-01-06 23:39:38 +0100

    vecs

--- rustlings/exercises/05_vecs/vecs1.rs
+++ rustlings/exercises/05_vecs/vecs1.rs
@@ -1,9 +1,13 @@
+use ::std::vec;
+
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // Array
 
     // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
     // Use the vector macro.
     // let v = ???;
+    let v = vec![10, 20, 30, 40];
+    //let v = Vec::from(a);
 
     (a, v)
 }
