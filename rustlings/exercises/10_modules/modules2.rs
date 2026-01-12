93a5adef8ae99baa71ada5575c0698b44de0beca
David Hinrichs <dvd.hnrchs@gmail.com>
2026-01-12 20:23:16 +0100

    modules

--- rustlings/exercises/10_modules/modules2.rs
+++ rustlings/exercises/10_modules/modules2.rs
@@ -1,26 +1,24 @@
 // You can bring module paths into scopes and provide new names for them with
 // the `use` and `as` keywords.
 
-mod delicious_snacks {
+pub mod delicious_snacks {
     // TODO: Add the following two `use` statements after fixing them.
     // use self::fruits::PEAR as ???;
     // use self::veggies::CUCUMBER as ???;
 
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &str = "Pear";
         pub const APPLE: &str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
 }
+use delicious_snacks::fruits::PEAR as fruit;
+use delicious_snacks::veggies::CUCUMBER as veggie;
 
 fn main() {
-    println!(
-        "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie,
-    );
+    println!("favorite snacks: {} and {}", fruit, veggie,);
 }
