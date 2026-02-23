9e8e88431c57726ae6c0cac665f8ac165d3df5ff
xstraven <7582591+xstraven@users.noreply.github.com>
2026-02-24 00:19:02 +0100

    fix errors1

--- rustlings/exercises/13_error_handling/errors1.rs
+++ rustlings/exercises/13_error_handling/errors1.rs
@@ -4,12 +4,12 @@
 // construct to `Option` that can be used to express error conditions. Change
 // the function signature and body to return `Result<String, String>` instead
 // of `Option<String>`.
-fn generate_nametag_text(name: String) -> Option<String> {
+fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
         // Empty names aren't allowed
-        None
+        Err("Empty names aren't allowed".into())
     } else {
-        Some(format!("Hi! My name is {name}"))
+        Ok(format!("Hi! My name is {name}"))
     }
 }
 
@@ -25,7 +25,7 @@ mod tests {
     fn generates_nametag_text_for_a_nonempty_name() {
         assert_eq!(
             generate_nametag_text("Beyoncé".to_string()).as_deref(),
-            Ok("Hi! My name is Beyoncés"),
+            Ok("Hi! My name is Beyoncé"),
         );
     }
 
