4437a62651c3cea61b0b7b8a5dc3f56db7b728f0
xstraven <7582591+xstraven@users.noreply.github.com>
2026-03-04 00:29:19 +0100

    finish iterators

--- rustlings/exercises/18_iterators/iterators4.rs
+++ rustlings/exercises/18_iterators/iterators4.rs
@@ -10,6 +10,8 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+    let iter = (1..=num).into_iter();
+    iter.product::<u64>()
 }
 
 fn main() {
